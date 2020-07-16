e187cf568730b23402b327790a715964bce59d9d
Maks Pazuniak <mpazuniak@gmail.com>
2020-07-16 18:47:00 -0400

    Added Auth & Registration libraries

--- quoraclone/settings.py
+++ quoraclone/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'users',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',
+    'rest_auth.registration',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom User Model
+AUTH_USER_MODEL = "users.CustomUser"
+
+# Django Crispy Forms
+CRISPY_TEMPLATE_PACK = "bootrap4"
+
+# django.contrib.sites
+SITE_ID = 1
+
+#django.allauth
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_REQUIRED = (True)
