cf35946e89ecb33566acd4496737614d6a815011
Maks Pazuniak <mpazuniak@gmail.com>
2020-07-16 18:59:26 -0400

    Add registration / loging URLs

--- quoraclone/urls.py
+++ quoraclone/urls.py
@@ -14,8 +14,43 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from django_registration.backends.one_step.views import RegistrationView
+
+from users.forms import CustomUserForm
+
+#https://django-registration.readthedocs.io/en/3.0/activation-workflow.html
 
 urlpatterns = [
+
+    # Admin URL
     path('admin/', admin.site.urls),
+
+    #  Register (create new accounts) via browser (Custom version)
+    path('accounts/register',
+        RegistrationView.as_view(
+            form_class=CustomUserForm,
+            success_url='/',
+            ), name='django_registration_register'),
+
+    # URL used by Django registration package
+    path('accounts/',
+        include('django_registration.backends.one_step.urls')),
+
+    # Login URLS used by Django to login user via browser
+    path('accounts/',
+        include('django.contrib.auth.urls')),
+
+    # Login via browsable API
+    path('api-auth/',
+        include('rest_framework.urls')),
+
+    # Login via REST
+    path('api/rest-auth.',
+        include('rest_auth.urls')),
+
+    # Register via REST
+    path('api-auth/rest-auth/registration/',
+        include('rest_auth.registration.urls'))
 ]
